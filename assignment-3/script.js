4544677a026ae522e1a7a8d4ebfc204cd9f21dbc
Dmitriy Melnichenko <melnichenko.dmytro@gmail.com>
2021-10-06 22:31:52 +0300

    Add optional second parameter to myFilter function, complete second task.

--- assignment-3/script.js
+++ assignment-3/script.js
@@ -1,11 +1,27 @@
-Array.prototype.myFilter = function (callback) {
+Array.prototype.myFilter = function (callback, thisArg) {
   let filtered = [];
 
-  for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) {
-      filtered.push(this[i]);
+  if (!Array.isArray(thisArg)) {
+    thisArg = this;
+  }
+
+  for (let i = 0; i < thisArg.length; i++) {
+    if (callback(thisArg[i], i, thisArg)) {
+      filtered.push(thisArg[i]);
     } 
   }
-  
+
   return filtered;
 };
+
+function createDebounceFunction(func, delay) {
+  let timeoutID;
+  
+  return function(...args) {
+    if(timeoutID) {
+      clearTimeout(timeoutID);
+    }
+
+    timeoutID = setTimeout(() => func(...args), delay);
+  };
+}
