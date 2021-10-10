f7f86fdf7419b26c88fb3a5b1a7b0ca2ccca60d4
Dmitriy Melnichenko <melnichenko.dmytro@gmail.com>
2021-10-10 19:19:19 +0300

    Complete second task.

--- assignment-4/script.js
+++ assignment-4/script.js
@@ -1,6 +1,42 @@
 function concatStrings(str, separator) {
   return function(nextStr) {
-    if(typeof nextStr !== 'string' || nextStr === 0) return str;
-      return concatStrings(`${str}${typeof separator==='string' ? separator : ''}${nextStr}`, separator)
+    if(typeof nextStr !== 'string' || nextStr.length === 0) {
+      return str;
+    }
+    return concatStrings(`${str}${typeof separator==='string' ? separator : ''}${nextStr}`, separator)
   }
 }
+
+
+class Calculator {
+  constructor(x, y) {
+    if(!Number.isInteger(x) || !Number.isInteger(y)) {
+      throw new Error('');
+    }
+    this.x = x;
+    this.y = y;
+
+    this.logSum = this.logSum.bind(this);
+    this.logDiv = this.logDiv.bind(this);
+    this.setX = this.setX.bind(this);
+    this.setY = this.setY.bind(this);
+  }
+
+  logSum(){
+    console.log(this.x + this.y);
+  }
+
+  logDiv() {
+    console.log(Math.round(this.x / this.y));
+  }
+
+  setX(value) {
+    this.x = value;
+  }
+
+  setY(value) {
+    this.y = value;
+  }
+
+}
+
